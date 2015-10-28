83c9b2a74dbc8273eb08a192575bb3a08428183c
stecb <stefano.ceschib@gmail.com>
2015-10-28 21:31:49 +0100

    updated examples + refactored components

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CiaoHandler, BoxHandler, BoxWithStateHandler, ClickableCounterHandler, InputHandler } from './components/examples';
+
 import { Router, Route, Link } from 'react-router';
 
-export class Ciao extends React.Component {
-  render() {
-    return (
-      <div>Ciao, {this.props.name}</div>
-    );
-  }
-}
+const LINKS = ['example_1', 'example_2', 'example_3', 'example_4', 'example_5'];
+const STYLE_UL = { marginBottom: '20px' }
 
-export class Box extends React.Component {
+class App extends React.Component {
+  renderLink(l, path) {
+    const link = `/${l}`;
+    if (path === link) {
+      return <li role="presentation" className="active" key={l}><a href="javascript:void(0)">{l}</a></li>
+    } else {
+      return <li key={l} role="presentation"><Link to={link}>{l}</Link></li>
+    }
+  }
   render() {
-    const list = this.props.list.map(item => <li key={item}>{item}</li>);
+    const path = this.props.location.pathname;
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <ul>
-          {list}
+      <div className='container'>
+        <h1><Link to='/'>⚛ React examples</Link></h1>
+        <ul className="nav nav-pills" style={STYLE_UL}>
+          {LINKS.map(l => this.renderLink(l, path))}
         </ul>
+        {this.props.children}
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<div>
-  <Ciao name='Stefanissimo' />
-  <Box title="Cool Box" list={['item 1', 'item 2', 'item 3', 'item N']} />
-  </div>, document.getElementById('app'))
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="/example_1" component={CiaoHandler} />
+    <Route path="/example_2" component={BoxHandler} />
+    <Route path="/example_3" component={BoxWithStateHandler} />
+    <Route path="/example_4" component={ClickableCounterHandler} />
+    <Route path="/example_5" component={InputHandler} />
+  </Route>
+);
+
+ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'));
